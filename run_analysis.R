37be8047bbccbd2a3acde000fa5994a4646a4d85
Yonggang Jing <eightwind@163.com>
2015-05-14 15:24:25 +0800

    R script of the assignment

--- run_analysis.R
+++ run_analysis.R
@@ -19,12 +19,12 @@ subject_merged<-rbind(subject_train,subject_test)
 
 # Extract the mean and std of the data
 features<-read.table("UCI HAR Dataset/features.txt")
-themean<-sapply(features[,2],function(x) grepl("mean()",x,fixed=TRUE))
+themean<-sapply(features[,2], function(x) grepl("mean()", x, fixed=TRUE))
 thestd<-sapply(features[,2], function(x) grepl("std()", x, fixed=TRUE))
 x_meanstd<-x_merged[,(themean | thestd)]
 colnames(x_meanstd)<-features[(themean | thestd),2]
 
-# Naming the activities uses descriptive activity
+# Name the activities uses descriptive activity
 activity_labels<-read.table("UCI HAR Dataset/activity_labels.txt")
 activity_labels<-activity_labels[y_merged[,1],2]
 y_merged[,1]<-activity_labels
@@ -32,9 +32,9 @@ colnames(y_merged)<-"activity"
 colnames(subject_merged)<-"subject"
 
 # Combine to a dataset and get mean of each variable by activity and subject
-combined<-cbind(subject_merged,y_merged,x_meanstd)
-tidy_dataset<-aggregate(combined[,3:length(combined)], by=list(subject=combined$subject,activity=combined$activity), mean)
+combined<-cbind(subject_merged, y_merged, x_meanstd)
+tidy_dataset<-aggregate(combined[,3:length(combined)], by=list(subject=combined$subject, activity=combined$activity), mean)
 
 # Submit the second tidy dataset
-write.table(tidy_dataset,"tidy_dataset.txt",row.names=FALSE)
+write.table(tidy_dataset, "tidy_dataset.txt", row.names=FALSE)
 message("Done!")
